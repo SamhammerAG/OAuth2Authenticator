01feabd1348be5cc603350eb29446a646dd06985
Dominik F <dfuchs@samhammer.de>
2022-05-24 11:58:51 +0200

    Move validator methods to extension

---
+++ OAuth2Authenticator.Tests/OAuth2TokenResponseExtensionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace OAuth2Authenticator.Tests
+{
+    public class OAuth2TokenResponseExtensionTest : BaseUnitTest
+    {
+        [Test]
+        public void Successful()
+        {
+            Assert.IsTrue(new OAuth2TokenResponse().Successful());
+            Assert.IsTrue(new OAuth2TokenResponse { Error = string.Empty }.Successful());
+
+            OAuth2TokenResponse token = null;
+            Assert.IsFalse(token.Successful());
+        }
+
+        [Test]
+        public void Valid()
+        {
+            Assert.IsTrue(new OAuth2TokenResponse
+            {
+                IssueDate = DateTime.Now,
+                ExpiresIn = 300
+            }.Valid());
+
+            Assert.IsFalse(new OAuth2TokenResponse
+            {
+                IssueDate = DateTime.Now.Subtract(TimeSpan.FromSeconds(10)),
+                ExpiresIn = 20
+            }.Valid(10));
+
+            Assert.IsFalse(new OAuth2TokenResponse
+            {
+                IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10)),
+                ExpiresIn = 300
+            }.Valid());
+        }
+    }
+}
---
+++ OAuth2Authenticator/Extensions/OAuth2TokenResponseExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OAuth2Authenticator
+{
+    public static class OAuth2TokenResponseExtension
+    {
+        /// <summary>
+        /// Checks that the response is present, and no error code is included.
+        /// </summary>
+        /// <param name="token">Token</param>
+        /// <returns>Successful</returns>
+        public static bool Successful(this OAuth2TokenResponse token)
+        {
+            return token != null && string.IsNullOrEmpty(token.Error);
+        }
+
+        /// <summary>
+        /// Checks that the token is not expired.
+        /// </summary>
+        /// <param name="token">Token</param>
+        /// <param name="threshold">The given threshold in seconds gets removed of the token life span. So the token expires before the actual expiration time.</param>
+        /// <returns>Valid</returns>
+        public static bool Valid(this OAuth2TokenResponse token, int threshold = 0)
+        {
+            return token.IssueDate.AddSeconds(token.ExpiresIn - threshold) >= DateTime.Now;
+        }
+    }
+}
