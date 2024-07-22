ecf27cf69b47913cae7446d15301916f5cebe903
Ott <ottalexanderdev@gmail.com>
2024-07-22 08:05:35 +0200

    AID-23806: Bumped packages

--- OAuth2Authenticator.Tests/OAuth2TokenHandlerTest.cs
+++ OAuth2Authenticator.Tests/OAuth2TokenHandlerTest.cs
@@ -7,137 +7,136 @@ using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using OAuth2Authenticator.Internal;
 
-namespace OAuth2Authenticator.Tests
+namespace OAuth2Authenticator.Tests;
+
+public class OAuth2TokenHandlerTest : BaseUnitTest
 {
-    public class OAuth2TokenHandlerTest : BaseUnitTest
+    private IHandlerAuthenticator _authenticator;
+    private ILogger<OAuth2TokenHandler> _logger;
+
+    private OAuth2TokenHandler _handler;
+
+    [SetUp]
+    public void Setup()
     {
-        private IHandlerAuthenticator _authenticator;
-        private ILogger<OAuth2TokenHandler> _logger;
+        _authenticator = A.Fake<IHandlerAuthenticator>();
+        _logger = A.Fake<ILogger<OAuth2TokenHandler>>();
 
-        private OAuth2TokenHandler _handler;
+        _handler = new OAuth2TokenHandler(
+            _authenticator,
+            _logger);
+    }
 
-        [SetUp]
-        public void Setup()
+    [Test]
+    public async Task RefreshHandlerValidToken()
+    {
+        var token = FillObject<OAuth2TokenResponse>();
+        token.IssueDate = DateTime.Now;
+        token.ExpiresIn = 300;
+
+        var result = await RefreshHandler(token);
+
+        result.Should().Be(token);
+    }
+
+    [Test]
+    public async Task RefreshHandlerEmptyToken()
+    {
+        var result = await RefreshHandler(new OAuth2TokenResponse());
+
+        A.CallTo(() => _authenticator.PasswordGrant(
+            A<string>.Ignored,
+            A<string>.Ignored,
+            A<string>.Ignored,
+            A<string>.Ignored,
+            A<CancellationToken>.Ignored)).MustHaveHappened();
+
+        result.Should().NotBeNull();
+    }
+
+    [Test]
+    public async Task RefreshHandlerRefreshToken()
+    {
+        var token = FillObject<OAuth2TokenResponse>();
+        token.IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10));
+        token.ExpiresIn = 300;
+
+        var result = await RefreshHandler(token);
+
+        A.CallTo(() => _authenticator.RefreshTokenGrant<OAuth2TokenResponse>(
+            A<string>.Ignored,
+            A<string>.Ignored,
+            token.RefreshToken,
+            A<CancellationToken>.Ignored)).MustHaveHappened();
+
+        token.Should().NotBeNull();
+        token.AccessToken.Should().NotBe(result.AccessToken);
+    }
+
+    [Test]
+    public async Task RefreshHandlerInvalidGrantError()
+    {
+        var token = FillObject<OAuth2TokenResponse>();
+        token.IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10));
+        token.ExpiresIn = 300;
+
+        A.CallTo(() => _authenticator.RefreshTokenGrant<OAuth2TokenResponse>(
+            A<string>.Ignored,
+            A<string>.Ignored,
+            token.RefreshToken,
+            A<CancellationToken>.Ignored)).Returns(new OAuth2TokenResponse
         {
-            _authenticator = A.Fake<IHandlerAuthenticator>();
-            _logger = A.Fake<ILogger<OAuth2TokenHandler>>();
+            Error = OAuth2ResponseErrors.InvalidGrant
+        });
 
-            _handler = new OAuth2TokenHandler(
-                _authenticator,
-                _logger);
-        }
+        var result = await RefreshHandler(token);
 
-        [Test]
-        public async Task RefreshHandlerValidToken()
+        A.CallTo(() => _authenticator.PasswordGrant(
+            A<string>.Ignored,
+            A<string>.Ignored,
+            A<string>.Ignored,
+            A<string>.Ignored,
+            A<CancellationToken>.Ignored)).MustHaveHappened();
+
+        result.Should().NotBeNull();
+        token.AccessToken.Should().NotBe(result.AccessToken);
+    }
+
+    [Test]
+    public async Task RefreshHandlerError()
+    {
+        var token = FillObject<OAuth2TokenResponse>();
+        token.IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10));
+        token.ExpiresIn = 300;
+
+        A.CallTo(() => _authenticator.RefreshTokenGrant<OAuth2TokenResponse>(
+            A<string>.Ignored,
+            A<string>.Ignored,
+            token.RefreshToken,
+            A<CancellationToken>.Ignored)).Returns(new OAuth2TokenResponse
         {
-            var token = FillObject<OAuth2TokenResponse>();
-            token.IssueDate = DateTime.Now;
-            token.ExpiresIn = 300;
+            Error = GetRandomString()
+        });
 
-            var result = await RefreshHandler(token);
+        var result = await RefreshHandler(token);
 
-            result.Should().Be(token);
-        }
+        result.Should().BeNull();
+    }
 
-        [Test]
-        public async Task RefreshHandlerEmptyToken()
-        {
-            var result = await RefreshHandler(new OAuth2TokenResponse());
-
-            A.CallTo(() => _authenticator.PasswordGrant(
-                A<string>.Ignored,
-                A<string>.Ignored,
-                A<string>.Ignored,
-                A<string>.Ignored,
-                A<CancellationToken>.Ignored)).MustHaveHappened();
-
-            result.Should().NotBeNull();
-        }
-
-        [Test]
-        public async Task RefreshHandlerRefreshToken()
-        {
-            var token = FillObject<OAuth2TokenResponse>();
-            token.IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10));
-            token.ExpiresIn = 300;
-
-            var result = await RefreshHandler(token);
-
-            A.CallTo(() => _authenticator.RefreshTokenGrant<OAuth2TokenResponse>(
-                A<string>.Ignored,
-                A<string>.Ignored,
-                token.RefreshToken,
-                A<CancellationToken>.Ignored)).MustHaveHappened();
-
-            token.Should().NotBeNull();
-            token.AccessToken.Should().NotBe(result.AccessToken);
-        }
-
-        [Test]
-        public async Task RefreshHandlerInvalidGrantError()
-        {
-            var token = FillObject<OAuth2TokenResponse>();
-            token.IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10));
-            token.ExpiresIn = 300;
-
-            A.CallTo(() => _authenticator.RefreshTokenGrant<OAuth2TokenResponse>(
-                A<string>.Ignored,
-                A<string>.Ignored,
-                token.RefreshToken,
-                A<CancellationToken>.Ignored)).Returns(new OAuth2TokenResponse
+    private async Task<OAuth2TokenResponse> RefreshHandler(OAuth2TokenResponse token)
+    {
+        return await _handler.RefreshHandler(
+            token,
+            "https://youtu.be/dQw4w9WgXcQ",
+            GetRandomString(),
+            async (url, clientId, cancellationToken) =>
             {
-                Error = OAuth2ResponseErrors.InvalidGrant
+                return await _authenticator.PasswordGrant(
+                    GetRandomString(),
+                    GetRandomString(),
+                    GetRandomString(),
+                    GetRandomString(),
+                    cancellationToken);
             });
-
-            var result = await RefreshHandler(token);
-
-            A.CallTo(() => _authenticator.PasswordGrant(
-                A<string>.Ignored,
-                A<string>.Ignored,
-                A<string>.Ignored,
-                A<string>.Ignored,
-                A<CancellationToken>.Ignored)).MustHaveHappened();
-
-            result.Should().NotBeNull();
-            token.AccessToken.Should().NotBe(result.AccessToken);
-        }
-
-        [Test]
-        public async Task RefreshHandlerError()
-        {
-            var token = FillObject<OAuth2TokenResponse>();
-            token.IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10));
-            token.ExpiresIn = 300;
-
-            A.CallTo(() => _authenticator.RefreshTokenGrant<OAuth2TokenResponse>(
-                A<string>.Ignored,
-                A<string>.Ignored,
-                token.RefreshToken,
-                A<CancellationToken>.Ignored)).Returns(new OAuth2TokenResponse
-            {
-                Error = GetRandomString()
-            });
-
-            var result = await RefreshHandler(token);
-
-            result.Should().BeNull();
-        }
-
-        private async Task<OAuth2TokenResponse> RefreshHandler(OAuth2TokenResponse token)
-        {
-            return await _handler.RefreshHandler(
-                token,
-                "https://youtu.be/dQw4w9WgXcQ",
-                GetRandomString(),
-                async (url, clientId, cancellationToken) =>
-                {
-                    return await _authenticator.PasswordGrant(
-                        GetRandomString(),
-                        GetRandomString(),
-                        GetRandomString(),
-                        GetRandomString(),
-                        cancellationToken);
-                });
-        }
     }
 }
--- OAuth2Authenticator.Tests/OAuth2TokenResponseExtensionTest.cs
+++ OAuth2Authenticator.Tests/OAuth2TokenResponseExtensionTest.cs
@@ -3,45 +3,44 @@ using FluentAssertions;
 using NUnit.Framework;
 using OAuth2Authenticator.Extensions;
 
-namespace OAuth2Authenticator.Tests
+namespace OAuth2Authenticator.Tests;
+
+public class OAuth2TokenResponseExtensionTest : BaseUnitTest
 {
-    public class OAuth2TokenResponseExtensionTest : BaseUnitTest
+    [Test]
+    public void Successful()
     {
-        [Test]
-        public void Successful()
+        new OAuth2TokenResponse().Successful().Should().BeTrue();
+        new OAuth2TokenResponse {Error = string.Empty}.Successful().Should().BeTrue();
+
+        OAuth2TokenResponse token = null;
+        token.Successful().Should().BeFalse();
+    }
+
+    [Test]
+    public void Valid()
+    {
+        new OAuth2TokenResponse
         {
-            new OAuth2TokenResponse().Successful().Should().BeTrue();
-            new OAuth2TokenResponse {Error = string.Empty}.Successful().Should().BeTrue();
+            IssueDate = DateTime.Now,
+            ExpiresIn = 300
+        }.Valid().Should().BeTrue();
 
-            OAuth2TokenResponse token = null;
-            token.Successful().Should().BeFalse();
-        }
-
-        [Test]
-        public void Valid()
+        new OAuth2TokenResponse
         {
-            new OAuth2TokenResponse
-            {
-                IssueDate = DateTime.Now,
-                ExpiresIn = 300
-            }.Valid().Should().BeTrue();
+            IssueDate = DateTime.Now.Subtract(TimeSpan.FromSeconds(10)),
+            ExpiresIn = 20
+        }.Valid(10).Should().BeFalse();
 
-            new OAuth2TokenResponse
-            {
-                IssueDate = DateTime.Now.Subtract(TimeSpan.FromSeconds(10)),
-                ExpiresIn = 20
-            }.Valid(10).Should().BeFalse();
+        new OAuth2TokenResponse
+        {
+            IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10)),
+            ExpiresIn = 300
+        }.Valid().Should().BeFalse();
 
-            new OAuth2TokenResponse
-            {
-                IssueDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(10)),
-                ExpiresIn = 300
-            }.Valid().Should().BeFalse();
+        new OAuth2TokenResponse().Valid(10).Should().BeFalse();
 
-            new OAuth2TokenResponse().Valid(10).Should().BeFalse();
-
-            OAuth2TokenResponse token = null;
-            token.Valid().Should().BeFalse();
-        }
+        OAuth2TokenResponse token = null;
+        token.Valid().Should().BeFalse();
     }
 }
